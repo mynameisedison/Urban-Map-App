356dbc8988e2659a15908dcb0a610f5315af44af
Edison <edisontoole@gmail.com>
2019-08-17 16:17:45 -0400

    working on making markers appear based on a set of data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import ReactMapGL, {Marker} from "react-map-gl";
+import * as parkData from "./skateboard-parks.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    viewport: {
+      width: '100vw',
+      height: '100vh',
+      latitude: 30.331528,
+      longitude: -81.661377,
+      zoom: 10
+    }
+  };
+  render(){
+    console.log(parkData.features[0].geometry.coordinates[0]);
+    return (
+      <div className="App">
+        <ReactMapGL {...this.state.viewport}
+        mapboxApiAccessToken={"pk.eyJ1IjoiZWRpc29udG9vbGUiLCJhIjoiY2pncXdnajM2MGg2ejJ4cGUzdW92bDNzcCJ9.YG4_JLO78bqmlpBcLHzuWw"}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapStyle = "mapbox://styles/edisontoole/cjzcqcbwh2eml1co2qsla3bbt">
+        {parkData.features.forEach((park)=>(
+          <Marker key={park.properties.PARK_ID} latitude={park.geometry.coordinates[1]} longitude={park.geometry.coordinates[0]}>
+          <button>
+            <img src="./logo192.png" alt="Skateboard"/>
+          </button>
+          </Marker>
+        ))}
+        </ReactMapGL>
+      </div>
+    );
+}
 }
 
 export default App;
